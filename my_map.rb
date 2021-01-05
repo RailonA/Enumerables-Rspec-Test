6a5e4618104ca20d717b171b8717a14c22d82f06
Mohammed <atefhamosa@gmail.com>
2021-01-06 00:52:20 +0200

    my_map

---
+++ my_map.rb
@@ -0,0 +1,9 @@
+module Enumerable
+    def my_map
+        raise 'NO BLOCK GIVEN!' unless block_given?
+        arr = to_a
+        arr.my_each_with_index do |_item, index|
+          arr[index] = yield arr[index]
+        end
+    end
+end
