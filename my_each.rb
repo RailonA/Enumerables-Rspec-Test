245f5a7b47bc1cda66cb14706189cd427243ac22
Mohammed <atefhamosa@gmail.com>
2021-01-05 19:55:36 +0200

    Make the method

---
+++ my_each.rb
@@ -0,0 +1,12 @@
+module Enumerable
+    def my_each
+        i =0
+        while i < arr.length
+            yield arr[i]
+            i +=1
+        end
+    end
+end
+
+numbers = [1,3,5]
+puts numbers.my_each {|x| puts x*x}
