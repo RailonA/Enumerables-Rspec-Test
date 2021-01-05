cafc7d06d797d0942dee4fa929b866983f549d83
Rodrigo Ibaceta <rodrigo.ibaceta01@gmail.com>
2021-01-05 18:19:52 -0300

    Create my_select

---
+++ my_select.rb
@@ -0,0 +1,15 @@
+require_relative 'my_each'
+
+module Enumerable
+  def my_select
+    raise 'NO BLOCK GIVEN!' unless block_given?
+
+    arr = to_a
+    index = 0
+
+    arr.my_each do |_item|
+      arr.delete_at(index) unless yield arr[index]
+      index += 1
+    end
+  end
+end
