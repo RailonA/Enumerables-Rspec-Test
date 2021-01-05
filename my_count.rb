3505afd039d716e46eaa722ccde17d2e7f890a1b
Rodrigo Ibaceta <rodrigo.ibaceta01@gmail.com>
2021-01-05 19:18:09 -0300

    Create my_count

--- my_count.rb
+++ my_count.rb
@@ -1,14 +1,16 @@
 require_relative 'my_each'
 
 module Enumerable
-  def my_select
-
+  def my_count(arg = nil)
     arr = to_a
+
+    return arr.length if arg.nil?
+
     index = 0
     count = 0
 
     arr.my_each do |_item|
-      
+      count += 1 if arg == arr[index]
       index += 1
     end
 
