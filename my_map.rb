d4f0fef1e0b1ad7b12edb434dab1a22e6a722f73
Mohammed <atefhamosa@gmail.com>
2021-01-06 00:53:01 +0200

    Update my_map.rb

--- my_map.rb
+++ my_map.rb
@@ -3,7 +3,7 @@ module Enumerable
         raise 'NO BLOCK GIVEN!' unless block_given?
         arr = to_a
         arr.my_each_with_index do |_item, index|
-          arr[index] = yield arr[index]
+            arr[index] = yield arr[index]
         end
     end
 end
