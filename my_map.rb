047abd7630a9b97107b8ad8d6f0aca31df8102c8
Mohammed <atefhamosa@gmail.com>
2021-01-06 17:52:32 +0200

    Update my_map.rb

--- my_map.rb
+++ my_map.rb
@@ -1,9 +1,19 @@
 module Enumerable
-    def my_map
-        raise 'NO BLOCK GIVEN!' unless block_given?
-        arr = to_a
-        arr.my_each_with_index do |_item, index|
-            arr[index] = yield arr[index]
-        end
-    end
-end
+      def my_map(pr=nil)
+          # raise 'NO BLOCK GIVEN!' unless block_given?
+          arr = to_a
+          if pr
+            arr.my_each_with_index do |_item, index|
+                arr[index] = proc.cal arr[index]
+            end
+          else
+            arr.my_each_with_index do |_item, index|
+                arr[index] = yield arr[index]
+            end
+          end
+      end
+  end
+  
+  pro = Proc.new{|x| x*2}
+  
+  [1,2,3].my_map(&pro)
