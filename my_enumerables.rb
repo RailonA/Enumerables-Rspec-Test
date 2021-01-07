9703c3b2e7cd694a178f1b8456cb6b0c01765844
Rodrigo Ibaceta <rodrigo.ibaceta01@gmail.com>
2021-01-07 16:49:39 -0300

    shorten methods

--- my_enumerables.rb
+++ my_enumerables.rb
@@ -1,6 +1,3 @@
-# rubocop:disable Metrics/ModuleLength
-# rubocop:disable Metrics/CyclomaticComplexity
-# rubocop:disable Metrics/PerceivedComplexity
 module Enumerable
   def my_each(pro = nil)
     return to_enum unless block_given?
@@ -78,24 +75,23 @@ module Enumerable
   end
 
   def my_any?(args = nil)
-    arr = to_a
     any = false
 
     if block_given?
-      arr.my_each_with_index do |_item, index|
+      my_each_with_index do |_item, index|
         any = true if yield arr[index]
       end
     elsif args.is_a? Class
-      arr.my_each_with_index do |item, _index|
+      my_each_with_index do |item, _index|
         puts item.class.ancestors
         any = true if item.class.ancestors.include?(args)
       end
     elsif args.is_a? Regexp
-      arr.my_each_with_index do |item, _index|
+      my_each_with_index do |item, _index|
         any = true if item.match(args)
       end
     else
-      arr.my_each_with_index do |item, _index|
+      my_each_with_index do |item, _index|
         any = true unless item == false || item.nil?
       end
     end
@@ -130,27 +126,19 @@ module Enumerable
     index = 0
     count = 0
 
-    if arg.is_a? Proc
-      while index < length
-        count += 1 if proc.call self[index]
-        index += 1
-      end
-    elsif block_given?
-      while index < length
-        count += 1 if yield self[index]
-        index += 1
-      end
-    elsif arg
-      while index < length
-        count += 1 if arg == self[index]
-        index += 1
-      end
-    else
-      my_each do |_item|
+    return length if arg.nil? && !block_given?
+
+    while index < length
+      if (arg.is_a?(Proc) && proc.call(self[index])) ||
+         (arg == self[index])
         count += 1
-        index += 1
+      elsif block_given?
+        count += 1 if yield self[index]
       end
+
+      index += 1
     end
+
     count
   end
 
@@ -172,41 +160,31 @@ module Enumerable
   end
 
   def my_inject(arg = self[0], sym = nil)
-    arr = to_a
     i = 0
     result = arg
 
     if arg.is_a? Symbol
+      i +=1
       sym = arg
-      result = arr[0]
+      result = self[0]
     end
 
-    if sym
-      while i < arr.length
-        result = result.send sym, arr[i]
-        i += 1
+    while i < length
+      if sym
+        p 'this'
+        result = result.send sym, self[i]
+      elsif arg && block_given?
+        p 'that'
+        result = yield result, self[i]
       end
-      result
-    elsif arg and block_given?
-      while i < arr.length
-        result = yield result, arr[i]
-        i += 1
-      end
-    else
-      i = 1
-      while i < arr.length
-        result = yield result, arr[i]
-        i += 1
-      end
+
+      i += 1
     end
+
     result
-  end
-
-  def multiply_els(arr = [])
-    arr.my_inject { |total, item| total * item }
   end
 end
 
-# rubocop:enable Metrics/ModuleLength
-# rubocop:enable Metrics/CyclomaticComplexity
-# rubocop:enable Metrics/PerceivedComplexity
+def multiply_els(arr = [])
+  arr.my_inject { |total, item| total * item }
+end
