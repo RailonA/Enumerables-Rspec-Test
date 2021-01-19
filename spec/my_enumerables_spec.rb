ab1709eb21143306b9a85a4634ca53e0e31d1d39
Railon <railonacosta@gmail.com>
2021-01-19 17:13:03 -0500

    Add test case for #my_each

--- spec/my_enumerables_spec.rb
+++ spec/my_enumerables_spec.rb
@@ -5,9 +5,18 @@ let(:my_array){[1, 2, 3, 4, 5]}
 let(:my_hash){ { x: 1, y: 2, z: 3 }}
 let(:my_text){ %w[ant bear cat]}
 let(:arg){ proc { |value| value * 2 }}
+let(:my_range){(1..9)}
+
+describe '#my_each' do 
+ it "Test .my_each to print out the same as .each" do
+    expect(my_array.my_each(&:even?)).to eq(my_array.each(&:even?))
+    expect(my_range.my_each(&:even?)).to eql(my_range.each(&:even?))
+    expect(my_hash.my_each{ |value|  value }).to eql(my_hash.each{ |value|  value })
+end 
+end
 
 
-
+ 
 end
 
 
