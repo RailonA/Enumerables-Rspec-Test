ca317c398e6e5f4738f97d9ec23774ba941b201b
Railon <railonacosta@gmail.com>
2021-01-19 17:19:23 -0500

    Add test case for my_each_with_index

--- spec/my_enumerables_spec.rb
+++ spec/my_enumerables_spec.rb
@@ -8,14 +8,20 @@ let(:arg){ proc { |value| value * 2 }}
 let(:my_range){(1..9)}
 
 describe '#my_each' do 
- it "Test .my_each to print out the same as .each" do
+ it "Test .my_each to return the same as .each" do
     expect(my_array.my_each(&:even?)).to eq(my_array.each(&:even?))
     expect(my_range.my_each(&:even?)).to eql(my_range.each(&:even?))
     expect(my_hash.my_each{ |value|  value }).to eql(my_hash.each{ |value|  value })
 end 
 end
 
-
+describe '#my_each_with_index' do 
+    it ' Test .my_each_with_index to return the same as .each_with_index' do 
+        expect(my_array.my_each_with_index{ |value|  value }).to eq(my_array.each_with_index{ |value|  value })
+    expect(my_range.my_each_with_index{ |value|  value }).to eql(my_range.each_with_index{ |value|  value })
+    expect(my_hash.my_each_with_index{ |value|  value }).to eql(my_hash.each_with_index{ |value|  value })
+    end
+end
  
 end
 
