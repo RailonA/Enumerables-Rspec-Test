4359578a014c4dfb691c00118c8bc442bd10b6db
Railon <railonacosta@gmail.com>
2021-01-19 16:50:46 -0500

    Add test cases for rspec

--- spec/my_enumerables_spec.rb
+++ spec/my_enumerables_spec.rb
@@ -1,2 +1,13 @@
 require_relative '../my_enumerables.rb'
 
+describe Enumerable do 
+let(:my_array){[1, 2, 3, 4, 5]}
+let(:my_hash){ { x: 1, y: 2, z: 3 }}
+let(:my_text){ %w[ant bear cat]}
+let(:arg){ proc { |value| value * 2 }}
+
+
+
+end
+
+
