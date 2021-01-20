bdf61ee99a50ec446cbe9a6122636faf39f8737a
Railon <railonacosta@gmail.com>
2021-01-20 10:12:07 -0500

    Add a test for each case in #my_each, #my_each_with_index, #my_select, #my_all?

--- spec/my_enumerables_spec.rb
+++ spec/my_enumerables_spec.rb
@@ -8,37 +8,61 @@ describe Enumerable do
   let(:my_range) { (1..9) }
 
   describe '#my_each' do
-    it 'Test .my_each to return the same as .each' do
+    it 'Test my_array.my_each to return the same as my_array.each' do
       expect(my_array.my_each(&:even?)).to eql(my_array.each(&:even?))
+    end
+    it 'Test my_range.my_each to return the same as my_range.each' do
       expect(my_range.my_each(&:even?)).to eql(my_range.each(&:even?))
+    end
+    it 'Test my_hash.my_each to return the same as my_hash.each' do
       expect(my_hash.my_each { |value| value }).to eql(my_hash.each { |value| value })
     end
   end
 
   describe '#my_each_with_index' do
-    it ' Test .my_each_with_index to return the same as .each_with_index' do
+    it ' Test my_array.my_each_with_index to return the same as my_array.each_with_index' do
       expect(my_array.my_each_with_index { |value| value }).to eql(my_array.each { |value| value })
+    end
+    it ' Test my_range.my_each_with_index to return the same as my_range.each_with_index' do
       expect(my_range.my_each_with_index { |value| value }).to eql(my_range.each { |value| value })
+    end
+    it ' Test my_hash.my_each_with_index to return the same as my_hash.each_with_index' do
       expect(my_hash.my_each_with_index { |value| value }).to eql(my_hash.each { |value| value })
     end
   end
 
   describe '#my_select' do
-    it ' Test .my_each_with_index to return the same as .each_with_index' do
+    it ' Test my_array.my_each_with_index to return the same as my_array.each_with_index' do
       expect(my_array.my_select { |value|  value }).to eql(my_array.select { |value| value })
+    end
+    it ' Test my_range.my_each_with_index to return the same as my_range.each_with_index' do
       expect(my_range.my_select { |value| value }).to eql(my_range.select { |value| value })
+    end
+    it ' Test my_hash.my_each_with_index to return the same as my_hash.each_with_index' do
       # expect(my_hash.my_select{  |key, value|   value }).to eql(my_hash.select{ |key, value|   value })
     end
   end
 
   describe '#my_all?' do
-    it ' Test .my_all? to return the same as .all?' do
+    it ' Test my_array.my_all? to return the same as my_array.all?' do
       expect(my_array.my_all? { |value| value }).to eql(my_array.all? { |value| value })
+    end
+    it ' Test my_array.my_all?(Numeric) to return the same as my_array.all?(Numeric)' do
       expect(my_array.my_all?(Numeric)).to eql(my_array.all?(Numeric))
+    end
+    it ' Test my_range.my_all? { |value| value } to return the same as my_range.all? { |value| value }' do
       expect(my_range.my_all? { |value| value }).to eql(my_range.all? { |value| value })
+    end
+    it ' Test my_range.my_all?(Numeric to return the same as my_range.all?(Numeric)' do
       expect(my_range.my_all?(Numeric)).to eql(my_range.all?(Numeric))
+    end
+    it ' Test my_hash.my_all? { |_key, value| value } to return the same as my_hash.all? { |_key, value| value }' do
       expect(my_hash.my_all? { |_key, value| value }).to eql(my_hash.all? { |_key, value| value })
+    end
+    it ' Test my_text.my_all? { |word| word.length >= 3 } to return the same as my_text.all? { |word| word.length >= 3 }' do
       expect(my_text.my_all? { |word| word.length >= 3 }).to eql(my_text.all? { |word| word.length >= 3 })
+    end
+    it ' Test my_text.my_all?(/t/) to return the same as my_text.all?(/t/)' do
       expect(my_text.my_all?(/t/)).to eql(my_text.all?(/t/))
     end
   end
